78bee55d400ea873e8a131f30ed3f76694309d55
Shravastee Thakur <shravastee@gmail.com>
2025-12-17 21:16:01 +0530

    db changes

--- Backend/config/connectdb.js
+++ Backend/config/connectdb.js
@@ -5,7 +5,18 @@ dotenv.config();
 
 const connectdb = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect(process.env.MONGO_URL, {
+      socketTimeoutMS: 45000,
+      connectTimeoutMS: 30000,
+      serverSelectionTimeoutMS: 30000,
+
+      maxPoolSize: 10,
+      minPoolSize: 2,
+      maxIdleTimeMS: 10000,
+
+      waitQueueTimeoutMS: 30000, // Timeout for operations waiting for a connection
+      maxTimeMS: 30000,
+    });
     logger.info("Databse connected");
   } catch (error) {
     logger.error("Database connection error", error);
