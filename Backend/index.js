34512d80ef7bfa55c33cf8444f9c9fe8fff621f7
Shravastee Thakur <shravastee@gmail.com>
2025-12-18 12:21:05 +0530

    Frontend URL changed

--- Backend/index.js
+++ Backend/index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://shra-stayease.netlify.app",
     credentials: true,
   })
 );
